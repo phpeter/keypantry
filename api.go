85b3004b43116239fbb3b5e98994c07566dffcab
Peter Forsee <peteref703@gmail.com>
2017-07-15 18:39:52 -0400

    Pw testing for API

--- api.go
+++ api.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"log"
 	"net/http"
 )
 
@@ -14,10 +15,24 @@ func apiHandler(db *sql.DB) func(res http.ResponseWriter, req *http.Request) {
 		username := req.Header.Get("Username")
 		password := req.Header.Get("Password")
 
+		row := db.QueryRow("SELECT passwordHash FROM users WHERE username=$1", username)
+		var userPw string
+		err := row.Scan(&userPw)
+
+		if err != nil {
+			log.Fatal("Error getting user password for API call " + err.Error())
+		}
+
+		if pwHash(password) == userPw {
+			res.Write([]byte("Correct pw"))
+		} else {
+			res.Write([]byte("Incorrect pw!!!!!!!"))
+		}
+
 		// x, _ := httputil.DumpRequest(req, true)
 		// log.Print(string(x))
 
-		res.Write([]byte(username + " - " + password))
+		log.Print(username + " - " + password)
 
 		// res.WriteHeader(200)
 	}
