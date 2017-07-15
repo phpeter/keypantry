04c91d9fda4f7fea479d1fe141c6920b1d7d97b5
Peter Forsee <peteref703@gmail.com>
2017-07-15 18:15:31 -0400

    Logging

--- api.go
+++ api.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -12,9 +13,12 @@ func apiHandler(db *sql.DB) func(http.ResponseWriter, *http.Request) {
 		// if credentials are correct, toggle item
 		//db.Query("UPDATE items WHERE user = user.id AND key = keyPressed SET own = NOT own")
 
+		log.Print("Connected to api handler")
+
 		for name, value := range res.Header() {
 			res.Write([]byte(fmt.Sprintf("%v: %v\n", name, value)))
 		}
+
 		res.WriteHeader(200)
 	}
 }
