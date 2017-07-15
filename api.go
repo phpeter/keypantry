dc83662d154fb6576dd2a11865890226189caf89
Peter Forsee <peteref703@gmail.com>
2017-07-15 18:33:32 -0400

    Log pw and username

--- api.go
+++ api.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"database/sql"
-	"log"
 	"net/http"
-	"net/http/httputil"
 )
 
 func apiHandler(db *sql.DB) func(res http.ResponseWriter, req *http.Request) {
@@ -13,8 +11,13 @@ func apiHandler(db *sql.DB) func(res http.ResponseWriter, req *http.Request) {
 		// if credentials are correct, toggle item
 		// db.Query("UPDATE items WHERE user = user.id AND key = keyPressed SET own = NOT own")
 
-		x, _ := httputil.DumpRequest(req, true)
-		log.Print(string(x))
+		username := req.Header.Get("Username")
+		password := req.Header.Get("Password")
+
+		// x, _ := httputil.DumpRequest(req, true)
+		// log.Print(string(x))
+
+		res.Write([]byte(username + " - " + password))
 
 		res.WriteHeader(200)
 	}
