d388bef2d57a484857a7883b9d77e907b607a28f
Peter Forsee <peteref703@gmail.com>
2017-07-15 18:51:12 -0400

    API now works

--- api.go
+++ api.go
@@ -4,13 +4,13 @@ import (
 	"database/sql"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func apiHandler(db *sql.DB) func(res http.ResponseWriter, req *http.Request) {
 	return func(res http.ResponseWriter, req *http.Request) {
-		// check credentials
-		// if credentials are correct, toggle item
-		// db.Query("UPDATE items WHERE user = user.id AND key = keyPressed SET own = NOT own")
+		// x, _ := httputil.DumpRequest(req, true)
+		// log.Print(string(x))
 
 		username := req.Header.Get("Username")
 		password := req.Header.Get("Password")
@@ -19,21 +19,25 @@ func apiHandler(db *sql.DB) func(res http.ResponseWriter, req *http.Request) {
 		var userPw string
 		err := row.Scan(&userPw)
 
-		if err == sql.ErrNoRows {
-			res.Write([]byte("Incorrect username!!!!!!!"))
-		}
-
-		if pwHash(password) == userPw {
-			res.Write([]byte("Correct pw"))
+		if err == sql.ErrNoRows || pwHash(password) != userPw {
+			res.Write([]byte("Bad credentials!"))
 		} else {
-			res.Write([]byte("Incorrect pw!!!!!!!"))
+
+			row := db.QueryRow("SELECT id FROM users WHERE username=$1", username)
+			var userID int
+			err := row.Scan(&userID)
+
+			if err != nil {
+				log.Print("Error, no user found")
+			}
+
+			path := req.URL.Path
+			params := strings.Split(path, "/")
+			keyPressed := params[len(params)-1]
+
+			db.Query("UPDATE items WHERE userID=$1 AND key=$2 SET isOwned = NOT isOwned", userID, keyPressed)
+			res.Write([]byte("Good!"))
 		}
 
-		// x, _ := httputil.DumpRequest(req, true)
-		// log.Print(string(x))
-
-		log.Print(username + " - " + password)
-
-		// res.WriteHeader(200)
 	}
 }
